b0d276567e70ae8efee9c2be35b3a9f3dd7c7ad0
anromeo <anthonykzullo@gmail.com>
2013-06-06 13:59:09 -0700

    changed controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Helpdesk::Application.routes.draw do
 
   resources :replies
   resources :tickets
-  resources :incoming_mails
+
   root to: "tickets#index"
 
   # The priority is based upon order of creation:
