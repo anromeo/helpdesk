1cb6404f7a7e2979dae8cf44ef3ba93b9e96cf07
anromeo <anthonykzullo@gmail.com>
2013-06-06 13:09:37 -0700

    send mail to ticket

--- app/mailers/tickets_mailer.rb
+++ app/mailers/tickets_mailer.rb
@@ -1,5 +1,5 @@
 class TicketsMailer < ActionMailer::Base
-  default to: "helpdesk@example.com"
+  default from: "helpdesk@example.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
@@ -10,6 +10,6 @@ class TicketsMailer < ActionMailer::Base
     @ticket = ticket
     @email = ticket.email
 
-    mail from: ticket.email, subject: ticket.subject
+    mail to: ticket.email, subject: ticket.subject + "Ticket (1)"
   end
 end
